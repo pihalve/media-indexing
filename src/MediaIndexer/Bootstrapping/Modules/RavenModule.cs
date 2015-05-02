ba3a836bc77244b0c8f940e9345e6f6b8a7a41a0
Mads Larsen <pihalve@gmail.com>
2015-05-02 23:26:32 +0200

    added missing files

---
+++ src/MediaIndexer/Bootstrapping/Modules/RavenModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Pihalve.MediaIndexer.Raven.Configuration;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Raven.Database.Server;
+
+namespace Pihalve.MediaIndexer.Bootstrapping.Modules
+{
+    public class RavenModule : BootModule
+    {
+        protected override void Register(ContainerBuilder builder)
+        {
+            builder.RegisterType<EmbeddableDocumentStore>().As<IDocumentStore>().SingleInstance();
+        }
+
+        public override void Configure(IContainer rootContainer)
+        {
+            var dataDirectory = Configuration.GetAppSetting<string>("DataDirectory");
+            var databaseServerPort = Configuration.GetAppSetting<int>("DatabaseServerPort");
+
+            NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(databaseServerPort);
+
+            DatabaseManager.CreateDatabaseIfNotExists(dataDirectory);
+
+            var store = (EmbeddableDocumentStore)rootContainer.Resolve<IDocumentStore>();
+            DatabaseManager.InitializeDatabase(dataDirectory, databaseServerPort, store);
+
+            //IndexCreation.CreateIndexes(GetType().Assembly, store);
+        }
+    }
+}
---
+++ src/MediaIndexer/Raven/Indexes/MediaItems_ByKeywords.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Pihalve.MediaIndexer.Entities;
+using Raven.Client.Indexes;
+
+namespace Pihalve.MediaIndexer.Raven.Indexes
+{
+    public class MediaItems_ByKeywords : AbstractIndexCreationTask<MediaItem>
+    {
+        public MediaItems_ByKeywords()
+        {
+            //Map = mediaItems => mediaItems
+            //    .Select(mediaItem => mediaItem.)
+        }
+    }
+}
