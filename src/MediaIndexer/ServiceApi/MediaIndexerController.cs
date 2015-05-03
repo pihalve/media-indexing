35c9f6161d08b25ede1114f81f03655aa75a0206
Mads Larsen <pihalve@gmail.com>
2015-05-03 16:48:10 +0200

    added QueryByTags method to webapi controller

--- src/MediaIndexer/ServiceApi/MediaIndexerController.cs
+++ src/MediaIndexer/ServiceApi/MediaIndexerController.cs
@@ -1,23 +1,58 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using log4net;
+using Pihalve.MediaIndexer.Entities;
+using Pihalve.MediaIndexer.Raven.Indexes;
+using Raven.Client;
+using Raven.Client.Linq;
 
 namespace Pihalve.MediaIndexer.ServiceApi
 {
     public class MediaIndexerController : ApiController
     {
-        private readonly IMediaItemImporter _bulkIndexer;
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private readonly IMediaItemImporter _importer;
+        private readonly IDocumentStore _store;
 
-        public MediaIndexerController(IMediaItemImporter bulkIndexer)
+        public MediaIndexerController(IMediaItemImporter importer, IDocumentStore store)
         {
-            _bulkIndexer = bulkIndexer;
+            _importer = importer;
+            _store = store;
         }
 
-        [Route("mediaindexer")]
-        public string Get()
+        [Route("mediaindexer/query")]
+        [HttpGet]
+        public HttpResponseMessage QueryByTags([ModelBinder]string[] tag)
         {
-            return "Hello world!";
+            try
+            {
+                using (var session = _store.OpenSession())
+                {
+                    RavenQueryStatistics stats;
+                    var results = session
+                        .Query<MediaItem, MediaItems_ByKeywords>()
+                        .Statistics(out stats)
+                        .Where(x => x.Keywords.ContainsAll(tag))
+                        .ToArray();
+
+                    if (Log.IsDebugEnabled)
+                    {
+                        Log.DebugFormat("QueryByTags '{0}' returned {1} results",
+                            tag != null ? tag.Aggregate((i, j) => i + "," + j) : "",
+                            stats.TotalResults);
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.OK, results);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         [Route("mediaindexer/import")]
@@ -26,14 +61,14 @@ namespace Pihalve.MediaIndexer.ServiceApi
         {
             try
             {
-                _bulkIndexer.Import();
+                _importer.Import();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Succeeded");
             }
             catch (Exception ex)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
             }
-
-            return Request.CreateResponse(HttpStatusCode.OK, "Succeeded");
         }
     }
 }
